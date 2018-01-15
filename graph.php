32d23539564d9e4999b67fb1009648971dc2461d
hudsonUriel <hudsonuriel@gmail.com>
2018-01-15 10:45:50 -0200

    Modification in graph.php and process.php to include OOP

--- graph.php
+++ graph.php
@@ -1,38 +1,41 @@
 <!DOCTYPE html>
 <html>
+  
   <head>
-    <title>Graphic</title>
+    <title>Arduino Graphic</title>
     <meta charset='UTF-8'>
-    <script src="js/graph.js"> </script>
-    
-    <style>
-      svg{
-          padding: 1%;
-      }  
-      
-    </style>
+    <script src='js/utils.js'></script>
+    <script src='js/graph.js'></script>
+    <script src='js/svg.js'></script>
+    <script src='js/graph-process.js'></script>
+    <link rel='stylesheet' href='css/style.css' type='text/css'/>
   </head>
   
   <body>
-    <p>Welcome to Graph</p>
     <div id='graph'>
-      <p id='report'></p>
-      <p id="reportPoint"></p>
-      
+
     </div>
     
-    <div id='conf-graph'>
+     <div id='conf-graph'>
         <form>
-          <p>Offset X: <input type="range" id="oX" onchange="graph_offset()"/></p>
-          <p>Offset Y: <input type="range" id="oY" onchange="graph_offset()"/></p>
+          <p>Offset X: <input type='range' id='oX' onchange='set_offset_value()'/></p>
+          <p>Offset Y: <input type='range' id='oY' onchange='set_offset_value()'/></p>
           
           <p>Amplitude: <input type='number' id='ampl'/></p>
-          <p>Internal Line Padding: <input type='number' min="0" id='linePadding' onchange="graph_lines()"/></p>
-          
-          
+          <p>Lateral Amplitude: <input type='number' id='latAmpl'/></p>
         </form>
+        
+        <p id='offsetData'></p>
+        <p id='graphData'></p>
     </div>
     
-
+    <?php
+      // starts session use
+      session_start();
+      
+      // creates the script tag with DOM SVG
+      echo $_SESSION['svgGraph'];
+      
+    ?>
   </body>
 </html>
--- process.php
+++ process.php
@@ -1,6 +1,28 @@
 <?php
+/*
+# The processing stage have __ phases.
+#
+# First, the data is getted by cookie
+# and then generates in to "data objects"
+#
+# Second, occurs the dynamic creation of
+# SVG elements (SVG and circle)
+*/
+
+/********** | PHASE 00 | **********
+********** opens classes  *********/
+
+// opens SVG
+require_once "./class/SVG.php";
+
+// starts session
+session_start();
+
+/********** | PHASE 01 | **********
+********* data processing *********/
+
 // recieves data
-$data = $_COOKIE["graphData"];
+$data = $_COOKIE["graph_data"];
 
 // turn data into array
 $data = explode(",", $data);
@@ -12,19 +34,34 @@ $type = $data[0];
 for($i=1; $i<sizeof($data); $i++)
     $data[$i] = floatval($data[$i]);
 
-// statistical analysis
-/*
-The statistical analysis will be made
-in the current account logged, saving the data:
-1) $data (cookie)
-2) avarage
-3) min, max, amplitude
-4) standar deviation
-*/
+/********** | PHASE 02 | **********
+*********** SVG creation **********/
 
-// send results do database
+// define properties
+$tag_name = 'svg';
+$id = 'svg_graph';
+$class_name = 'svgClass';
+$namespace = 'http://www.w3.org/2000/svg';
+$parent = 'document.getElementById("graph")';
 
-// call graph.php
+// creates new SVG
+$svgGraph = new SVG;
+
+$svgGraph->set_tag_name($tag_name);
+$svgGraph->set_id($id);
+$svgGraph->set_class_name($class_name);
+$svgGraph->set_namespace($namespace);
+$svgGraph->set_parent_element($parent);
+
+/********** | PHASE 03 | **********
+***********  stores data  *********/
+
+$_SESSION['svgGraph'] = $svgGraph->to_dom();
+
+/********** | PHASE 04 | **********
+*********  calls next page  *******/
+
+// calls graph.php
 header("Location: ./graph.php");
 
 ?>
