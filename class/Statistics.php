772cf5abd4a3a9872b7ee63a77c567fba33a958e
hudsonUriel <hudsonuriel@gmail.com>
2018-01-15 10:43:27 -0200

    Creation of Statistics class

---
+++ class/Statistics.php
@@ -0,0 +1,11 @@
+<?php
+class Statistics {
+  // define properties
+  private $statistic_data;
+  public $avarage;
+  
+  // define set
+  public function set_statistics_data($statistic_data){
+    $this->statistic_data = $statistic_data;
+  }
+}
