867e54d81aecaffba301a8bf1cebff39264766ea
hudsonUriel <hudsonuriel@gmail.com>
2018-01-16 20:50:26 -0200

    Remotion of unecessary .js file

--- js/conf.js
+++
@@ -1,9 +0,0 @@
-document.onload = function(){
-  var sizeERROR = document.getElementsByClassName('status_error').length;
-  var sizeOK = document.getElementsByClassName('status_correct').length;
-
-  for(i=0, j=0; i<sizeERROR, j<sizeOK; i++, j++){
-    document.getElementsByClassName('status_error')[i].value = "ERROR";
-    document.getElementsByClassName('status_correct')[j].value = "OK";
-  }
-}
