e3f99924b335e444233550306db08e9247115ad0
hudsonUriel <hudsonuriel@gmail.com>
2018-01-16 20:53:03 -0200

    Modifications in SVGData to extends Data Class and implements HTMLObject. This way, the SVGData is only a HTML connection between the statistic data and its representation in the SVG

--- class/SVGData.php
+++ class/SVGData.php
@@ -1,24 +1,9 @@
 <?php
 
-require_once("DrawableObject.php");
-require_once("StatisticsObject.php");
+require_once("Data.php");
 require_once("HTMLObject.php");
 
-class SVGData implements DrawableObject, StatisticsObject, HTMLObject{ 
-  // Drawable attriutes
-  private $x_position;
-  private $y_position;
-  private $fill;
-  private $fill_color;
-  private $outline;
-  private $outline_color;
-  private $outline_width;
-  private $draw_area;
-  
-  // Statistics attributes
-  private $key_value;
-  private $key_y;
-  
+class SVGData extends data implements HTMLObject{
   // HTML attributes
   private $tag_name;
   private $namespace;
@@ -27,46 +12,7 @@ class SVGData implements DrawableObject, StatisticsObject, HTMLObject{
   private $parent_element;
   
   // setters
-  function set_x_position($x_position) {
-    $this->x_position = $x_position;
-  }
-
-  function set_y_position($y_position) {
-    $this->y_position = $y_position;
-  }
-
-  function set_fill($fill) {
-    $this->fill = $fill;
-  }
-
-  function set_fill_color($fill_color) {
-    $this->fill_color = $fill_color;
-  }
-
-  function set_outline($outline) {
-    $this->outline = $outline;
-  }
-
-  function set_outline_color($outline_color) {
-    $this->outline_color = $outline_color;
-  }
-
-  function set_outline_width($outline_width) {
-    $this->outline_width = $outline_width;
-  }
-
-  function set_draw_area($drawArea) {
-    $this->drawArea = $drawArea;
-  }
-
-  function set_key_value($key_value) {
-    $this->key_value = $key_value;
-  }
-
-  function set_key_y($key_y) {
-    $this->key_y = $key_y;
-  }
-
+  
   function set_tag_name($tag_name) {
     $this->tag_name = $tag_name;
   }
@@ -86,48 +32,8 @@ class SVGData implements DrawableObject, StatisticsObject, HTMLObject{
   function set_parent_element($parent_element) {
     $this->parent_element = $parent_element;
   }
-
+  
   // getters
-  function get_x_position():float {
-    return $this->x_position;
-  }
-
-  function get_y_position():float {
-    return $this->y_position;
-  }
-
-  function get_fill():bool {
-    return $this->fill;
-  }
-
-  function get_fill_color():string {
-    return $this->fill_color;
-  }
-
-  function get_outline():bool {
-    return $this->outline;
-  }
-
-  function get_outline_color():string {
-    return $this->outline_color;
-  }
-
-  function get_outline_width():string {
-    return $this->outline_width;
-  }
-
-  function get_draw_area():DrawArea {
-    return $this->drawArea;
-  }
-
-  function get_key_value():float {
-    return $this->key_value;
-  }
-
-  function get_key_y():float {
-    return $this->key_y;
-  }
-
   function get_tag_name():string {
     return $this->tag_name;
   }
@@ -147,24 +53,9 @@ class SVGData implements DrawableObject, StatisticsObject, HTMLObject{
   function get_parent_element():string {
     return $this->parent_element;
   }
-
-  // to_string
-  public function to_dom():string {
-    return(
-        "\n\n\t$this->id="
-        . "document.createElementNS('$this->namespace', '$this->tag_name');" .
-        "\n\t$this->id.id='$this->id';" .
-        "\n\t$this->id.class='$this->class_name';" .
-        
-        "\n\t$this->id.setAttribute('fill', '#88d')" .
-        "\n\t$this->id.setAttribute('r', '15')" .
-        "\n\t$this->id.setAttribute('cx', '100')" .
-        "\n\t$this->id.setAttribute('cx', '100')" .
-        
-        "\n\t$this->parent_element.appendChild($this->id);"
-    );
-  }
-
+  
+  // to_html and to_dom
+  
   public function to_html():string {
     return(
       "<" . $this->tag_name .
@@ -175,15 +66,21 @@ class SVGData implements DrawableObject, StatisticsObject, HTMLObject{
       "</" . $this->tag_name .">"  
     );
   }
-
-  public function to_string():string {
+  
+  public function to_dom():string {
     return(
-        "TAG_NAME = " . $this->tag_name .
-        "<br/>ID = " . $this->id .
-        "<br/>CLASS = " . $this->class_name .
-        "<br/>HTML_PARENT = " . $this->parent_element
+        "\n\n\t$this->id="
+        . "document.createElementNS('$this->namespace', '$this->tag_name');" .
+        "\n\t$this->id.id='$this->id';" .
+        "\n\t$this->id.setAttribute('class', '$this->class_name')" .
+        "\n\t$this->id.setAttribute('fill', '#88d')" .
+        "\n\t$this->id.setAttribute('r', '5')" .
+        "\n\t$this->id.setAttribute('cx', '100')" .
+        "\n\t$this->id.setAttribute('cy', '100')" .
+        
+        "\n\t$this->parent_element.appendChild($this->id);"
     );
   }
 
-
+  
 }
