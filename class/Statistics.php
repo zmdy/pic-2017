a8ecdf71d09c16ae069af831a09eb90480fde112
hudsonUriel <hudsonuriel@gmail.com>
2018-01-16 11:22:20 -0200

    Modification in Statistics Classes to include new methods

--- class/Statistics.php
+++ class/Statistics.php
@@ -2,10 +2,32 @@
 class Statistics {
   // define properties
   private $statistic_data;
-  public $avarage;
+  public $average;
+  public $variance;
+  public $standard_deviation;
   
-  // define set
+  // define setters
   public function set_statistics_data($statistic_data){
     $this->statistic_data = $statistic_data;
   }
+  
+  // define getters
+  public function get_statistic_data():StatisticsObject {
+    return $this->statistic_datastatistic_data;
+  }
+  
+  public function get_average():float {
+    return $this->average;
+  }
+  
+  public function get_variance():float {
+    return $this->variance;
+  }
+  
+  public function get_standard_deviation():float {
+    return $this->standard_deviation;
+  }
+  
+  // define calculus
+  
 }
