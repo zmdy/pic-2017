f1fe658630b8721d59d2eaf5d1cff6edc9920e74
hudsonUriel <hudsonuriel@gmail.com>
2018-01-24 21:28:59 -0200

    Remove test file

--- testePOO.php
+++ testePOO.php
@@ -1,27 +1,7 @@
 <?php
-// opens SVG
-require_once "./class/SVG.php";
-
-// general properties
-$tag_name = 'SVG';
-$id = 'svgGraph';
-$class_name = 'svgClass';
-$namespace = 'http://www.w3.org/2000/svg';
-    
-// creates new SVG
-$svgGraph = new SVG;
-
-$svgGraph->set_tag_name($tag_name);
-$svgGraph->set_id($id);
-$svgGraph->set_class_name($class_name);
-$svgGraph->set_namespace($namespace);
+$val = -10;
 
 
-// shows svgGraph
-echo $svgGraph->to_html();
-
-echo $svgGraph->to_dom();
-
-
+echo "ABS IS " + abs(10);
 
 ?>
