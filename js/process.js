c5a57bea963314fd474beffd5080e35451217ca4
hudsonUriel <hudsonuriel@gmail.com>
2018-02-07 21:53:08 -0200

    Sporadic increments in process, to includes new functionalities to manual inputs

--- js/process.js
+++ js/process.js
@@ -91,7 +91,7 @@ function shows_insertion_area(object){
 }
 
 // creates table with data elements
-function reloadData(){
+function createData(){
   // defines creation area
   creationArea = document.getElementById('manual-insertion-area');
   
@@ -105,11 +105,16 @@ function reloadData(){
   tableTR = [];
   tableTD = [];
   inputData= [];
+  spanRemove = [];
+  spanCreate = [];
+  
+  //colors = colorPallete([250, 0, 0], [-10, 50, -50], channels);
   
   for(i=0; i<channels; i++){
     // creates new table
     tableData[i] = document.createElement('table');
     tableData[i].className = 'tableData';
+    tableData[i].id = 'channel_' + (i+1);
     
     // creates new tr (table row)
     tableRow[i] = document.createElement('tr');
@@ -118,10 +123,17 @@ function reloadData(){
     tableTitle[i] = document.createElement('th');
     tableTitle[i].innerHTML = 'CHANNEL ' + (i+1);
     
+    // creates button for add new input
+    spanCreate[i] = document.createElement('button');
+    spanCreate[i].innerHTML = 'NEW';
+    spanCreate[i].className = 'spanCreate';
+    spanCreate[i].onclick = createDataElement;
+    
     // appends
     creationArea.appendChild(tableData[i]);
     tableData[i].appendChild(tableRow[i]);
     tableRow[i].appendChild(tableTitle[i]);
+    tableTitle[i].appendChild(spanCreate[i]);
     
     // creates new td and input elements
     for(j=0; j<sizeSample; j++){
@@ -132,16 +144,68 @@ function reloadData(){
       // creates and configures new input
       inputData[j] = document.createElement('input');
       inputData[j].type = 'number';
+      inputData[j].className = 'inputData';
+      // inputData[j].style.background = 'rgb(' + colors[i] + ')';
+      
+      // creates and configures span
+      spanRemove[j] = document.createElement('button');
+      spanRemove[j].innerHTML = '&#x2717;';
+      spanRemove[j].className = 'spanRemove';
+      spanRemove[j].onclick = removeDataElement;
       
       // appends
       tableData[i].appendChild(tableTR[j]);
       tableTR[j].appendChild(tableTD[j]);
       tableTD[j].appendChild(inputData[j]);
+      tableTD[j].appendChild(spanRemove[j]);
     }
-    
-    
   }
   
-  console.log(creationArea);
-  alert(sizeSample + ' inputs by ' + channels + ' channels');
+  // don't show btnCreate
+  document.getElementById('btnCreate').style.display = 'none';
+}
+
+function removeDataElement(){
+  // parent table
+  parentTable = this.parentNode.parentNode.parentNode;
+  
+  // get the number of elements
+  if(parentTable.childElementCount > 2){
+    parent = this.parentNode.parentNode;
+    parent.parentNode.removeChild(parent);
+  } else{
+    confirm_excl = confirm('Confirms exclusion of ' + parentTable.id + '!!!');
+    
+    // if confirms
+    if(confirm_excl == true){
+      // remove current channel
+      parentTable.parentNode.removeChild(parentTable);
+    }
+  }   
+}
+
+function createDataElement(){
+  // get parent table
+  parentTable = this.parentNode.parentNode.parentNode;
+  
+  // new td and tr
+  tableTD = document.createElement('td');
+  tableTR = document.createElement('tr');
+
+  // creates and configures new input
+  inputData = document.createElement('input');
+  inputData.type = 'number';
+  inputData.className = 'inputData';
+
+  // creates and configures span
+  spanRemove = document.createElement('button');
+  spanRemove.innerHTML = '&#x2717;';
+  spanRemove.className = 'spanRemove';
+  spanRemove.onclick = removeDataElement;
+
+  // appends
+  parentTable.appendChild(tableTR);
+  tableTR.appendChild(tableTD);
+  tableTD.appendChild(inputData);
+  tableTD.appendChild(spanRemove);
 }
