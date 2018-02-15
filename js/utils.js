a088c4b382439c2924fb28083e64c79b012d0191
hudsonUriel <hudsonuriel@gmail.com>
2018-02-15 19:27:49 -0200

    Add function to convert number to different bases - under MIT license

--- js/utils.js
+++ js/utils.js
@@ -123,3 +123,68 @@ function parse_int(array){
   
   return array;
 }
+
+/*
+ * _____ CONVERT MODULE _____ *
+ * |
+ * |
+ * |--> This function is used to converts
+ * [...] values to different bases
+ * |
+ * |
+ * |--> USAGE
+ * |ConvertBase.bin2dec('111'); // '7'
+ * |ConvertBase.dec2hex('42'); // '2a'
+ * |ConvertBase.hex2bin('f8'); // '11111000'
+ * |ConvertBase.dec2bin('22'); // '10110'
+ * |
+ * |
+ * ___________________________________*
+ */
+(function(){
+
+    var ConvertBase = function (num) {
+        return {
+            from : function (baseFrom) {
+                return {
+                    to : function (baseTo) {
+                        return parseInt(num, baseFrom).toString(baseTo);
+                    }
+                };
+            }
+        };
+    };
+        
+    // binary to decimal
+    ConvertBase.bin2dec = function (num) {
+        return ConvertBase(num).from(2).to(10);
+    };
+    
+    // binary to hexadecimal
+    ConvertBase.bin2hex = function (num) {
+        return ConvertBase(num).from(2).to(16);
+    };
+    
+    // decimal to binary
+    ConvertBase.dec2bin = function (num) {
+        return ConvertBase(num).from(10).to(2);
+    };
+    
+    // decimal to hexadecimal
+    ConvertBase.dec2hex = function (num) {
+        return ConvertBase(num).from(10).to(16);
+    };
+    
+    // hexadecimal to binary
+    ConvertBase.hex2bin = function (num) {
+        return ConvertBase(num).from(16).to(2);
+    };
+    
+    // hexadecimal to decimal
+    ConvertBase.hex2dec = function (num) {
+        return ConvertBase(num).from(16).to(10);
+    };
+    
+    this.ConvertBase = ConvertBase;
+    
+})(this);
