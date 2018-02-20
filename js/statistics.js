3bfb8626dc188a84b8631411114432f070bb543a
hudsonUriel <hudsonuriel@gmail.com>
2018-02-20 12:12:28 -0300

    Add generic statistic function

--- js/statistics.js
+++ js/statistics.js
@@ -2,8 +2,28 @@
 Basic Statistic Calculations
  */
 
+/* 
+Basic Statistic Calculations
+ */
 
-function max(data){
-    min
+function statisticsProcess(){
+  // defines statData
+  var statData = new Statistics(data_points);  
+  
+  // calcs variables
+  statData.StatisticReport();
+  
+  // report
+  out = 
+      'size_sample = ' + statData.size +
+      ' | avarage = ' + statData.avr +
+      ' | min = ' + statData.min +
+      ' | max = ' + statData.max +
+      ' | variance = ' + statData.variance +
+      ' | &sigma; = ' + statData.s_deviation +
+      ' | variation_coefficient = ' + statData.var_coef;
+      //'\ndeviation = ' + statData.deviation;
+  
+  // shows
+  document.getElementById('statReport').innerHTML = out;
 }
-
